bf2f9d542b6637a9f661a8ffea227f20d250f9d8
XE <XE@XE4061>
2025-01-19 15:10:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿string variable_string = "Это строка";
+int variable_int = 12;
+double variable_double = 24.5;
+bool variable_bool = true;
+char variable_char = 'a';
+
+
+Console.WriteLine("Тип данных - строчка " + variable_string);
+Console.WriteLine("Тип данных - целочисленный " + variable_int);
+Console.WriteLine("Тип данных - дробный " + variable_double);
+Console.WriteLine("Тип данных - логический " + variable_bool);
+Console.WriteLine("Тип данных - символьный " + variable_char);
+
+Console.WriteLine(variable_int + variable_double);
+
+
+
+Console.WriteLine("Введите переменную типа int: ");
+int xInt;
+xInt = Int32.Parse(Console.ReadLine());
+Console.WriteLine(xInt + " - ваше значение.");
+
+Console.WriteLine("Введите переменную типа char: ");
+char xChar;
+xChar = Char.Parse(Console.ReadLine());
+Console.WriteLine(xChar + " - ваше значение.");
+
+Console.WriteLine("Введите переменную типа double: ");
+double xDouble;
+xDouble = Double.Parse(Console.ReadLine());
+Console.WriteLine(xDouble + " - ваше значение.");
+
+Console.WriteLine("Введите переменную типа bool: ");
+bool xBool;
+xBool = Boolean.Parse(Console.ReadLine());
+Console.WriteLine(xBool + " - ваше значение.");
+
+Console.WriteLine("Введите переменную типа string: ");
+string xString;
+xString = Console.ReadLine();
+Console.WriteLine(xDouble + " - ваше значение.");
+
