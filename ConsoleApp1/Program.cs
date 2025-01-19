4ea0f42ceb6e5abf0809bce232442fb26fe87570
soorek <soorek.yellow@gmail.com>
2025-01-19 15:18:29 +0300

    Добавлены комментарии в код

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,14 @@
-﻿string variable_string = "Это строка";
+﻿
+// Типы данных в C#
+
+string variable_string = "Это строка";
 int variable_int = 12;
 double variable_double = 24.5;
 bool variable_bool = true;
 char variable_char = 'a';
 
+
+// Консольный вывод
 
 Console.WriteLine("Тип данных - строчка " + variable_string);
 Console.WriteLine("Тип данных - целочисленный " + variable_int);
@@ -11,9 +16,11 @@ Console.WriteLine("Тип данных - дробный " + variable_double);
 Console.WriteLine("Тип данных - логический " + variable_bool);
 Console.WriteLine("Тип данных - символьный " + variable_char);
 
+// Сложение двух переменных в консоли для вывода
+
 Console.WriteLine(variable_int + variable_double);
 
-
+// Консольный ввод и конвертация для него типов данных
 
 Console.WriteLine("Введите переменную типа int: ");
 int xInt;
